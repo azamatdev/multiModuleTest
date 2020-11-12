e1e43f81c9e410cd26a841ef5f9be3414b20b4cd
Azamat Mahkamov <azamat.mahkamov.96@gmail.com>
2020-11-12 16:15:46 +0300

    Some Changes

--- myModule/src/main/java/uz/usoft/multimoduletest/MainActivity.kt
+++ myModule/src/main/java/uz/usoft/multimoduletest/MainActivity.kt
@@ -1,12 +1,20 @@
 package uz.usoft.multimoduletest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import uz.usoft.businessfeature.BusinessActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            startActivity(Intent(this, BusinessActivity::class.java))
+        }
+
 
     }
 }
